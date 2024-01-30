18fdfbfc48177c33b31450ff85f7f4b0a38220ac
Harsh Dugar <dugarharsh2050@gmail.com>
2024-01-30 19:29:41 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from "./pages/Home"
+
+const App = () => {
+  return (
+    <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter'>
+      
+      <Home/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/core/HomePage/HighLightText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HighLightText = ({text}) => {
+  return (
+    <span className='font-bold text-richblue-200 bg-gradient-to-r from-richblue-600 to-richblue-900'>
+        
+        {text}
+    </span>
+  )
+}
+
+export default HighLightText
