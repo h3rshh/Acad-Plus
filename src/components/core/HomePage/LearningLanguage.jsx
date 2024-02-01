087bcd39fa7452f0109c3ce8bbce01a96d050bba
Harsh Dugar <dugarharsh2050@gmail.com>
2024-02-01 19:56:16 +0530

    Lect 2 Half

---
+++ src/components/core/HomePage/LearningLanguage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LearningLanguage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LearningLanguage
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,11 @@ import {FaArrowRight} from "react-icons/fa"
 import { NavLink } from 'react-router-dom';
 import HighLightText from '../components/core/HomePage/HighLightText';
 import CTAButton from '../components/core/HomePage/Button';
+import Banner from "../assets/Images/banner.mp4"
+import CodeBlocks from "../components/core/HomePage/CodeBlocks"
+import "../App.css"
+import LearningLanguage from "../components/core/HomePage/LearningLanguage";
+import Timeline from "../components/core/HomePage/Timeline"
 
 const Home = () => {
 
@@ -43,12 +48,127 @@ const Home = () => {
                 </CTAButton>
             </div>
 
-        </div>
+            <div className='shadow-blue-200 mx-3 my-12'>
+                {/* <video  
+                muted
+                loop
+                autoPlay>
+                    <source src={Banner} type='video/mp4' />
+                </video> */}
+                <div className='bg-richblack-600 w-[62rem] h-[35rem]'></div>
+            </div>
+
+            { /* Code Sub Section 2 */}
+            <div>
+                <CodeBlocks 
+                    position={"lg:flex-row"}
+                    heading={
+                    <div>
+                        Unlock Your
+                        <HighLightText text={"Coding Potential"} />
+                        with our Online Courses
+                    </div>}
+                    subheading={"Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with You"}
+                    ctabtn1={{
+                        btnText: "try it yourself",
+                        linkto: "/signup",
+                        active: true,
+                    }}
+                    ctabtn2={{
+                        btnText: "learn more",
+                        linkto: "/login",
+                        active: false,
+                    }}
+                    codeColor={"text-yellow-25"}
+                    codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+                    backgroundGradient={<div className="codeblock1 absolute"></div>}
+                />
+            </div>
+
+            { /* Code Sub Section 3 */}
+            <div>
+                <CodeBlocks 
+                    position={"lg:flex-row-reverse"}
+                    heading={
+                    <div>
+                        Unlock Your
+                        <HighLightText text={"Coding Potential"} />
+                        with our Online Courses
+                    </div>}
+                    subheading={"Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with You"}
+                    ctabtn1={{
+                        btnText: "try it yourself",
+                        linkto: "/signup",
+                        active: true,
+                    }}
+                    ctabtn2={{
+                        btnText: "learn more",
+                        linkto: "/login",
+                        active: false,
+                    }}
+                    codeColor={"text-yellow-25"}
+                    codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+                    backgroundGradient={<div className="codeblock1 absolute"></div>}
+                />
+            </div>
+
+        </div>    
+
        {/* Section 2 */}
+       <div className='bg-pure-greys-5 text-richblack-700'>
+            <div className='homepage_bg h-[333px]'>
+                
+                <div className='w-11/12 max-w-maxContent flex flex-col items-center
+                gap-5 mx-auto justify-center'>
 
-       {/* Section 3 */}
+                    <div className='h-[250px]'></div>
 
-       {/* Section 4 */}
+                    <div  className='flex flex-row gap-7 text-white'>
+                        <CTAButton active={true} linkto={"/signup"}>
+                            <div className='flex items-center gap-3'>
+                                <p>Explore Full Catalog</p>
+                                <FaArrowRight />
+                            </div>                            
+                        </CTAButton>
+                        
+                        <CTAButton active={false} linkto={"/signup"}>
+                            <div className='flex items-center gap-3'>
+                                <p>Learn More</p>
+                            </div>                            
+                        </CTAButton>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className='mx-auto max-w-11/12 max-w-maxContent flex flex-col items-center justify-between'>
+                
+                <div className='flex flex-row gap-5 mb-10 mt-[95px]'>
+
+                    <div className='text-4xl font-semibold w-[45%]'>
+                        Get the skills you need for a
+                        <HighLightText text={"Job that is in Demand "}/>    
+                    </div>
+
+
+                    <div className='flex flex-col gap-10 w-[40%] items-start '>
+                        <p className='text-[16px]'>The modern Acad Zenith dictates it's own terms. Today, to be a
+                            competitive specialist, you need more than professional skills</p>
+
+                        <CTAButton active={true} linkto={"/signup"}>
+                            Learn More     
+                        </CTAButton>
+                    </div>
+
+                </div>
+
+                <Timeline />
+
+                <LearningLanguage />
+                
+            </div>
+
+       </div>
     </div>
   )
 }
