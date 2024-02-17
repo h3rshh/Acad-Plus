6c35cd4f1d576c1b271a6b6059742f7f3e2ebdcd
Harsh Dugar <dugarharsh2050@gmail.com>
2024-02-17 18:13:36 +0530

    Fixes

--- server/routes/PaymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { capturePayment, verifySignature } = require("../controllers/PaymentController");
-const { auth, isInstructor, isAdmin, isStudent } = require("../middlewares/authMiddleware");
-router.post("/capturepayment", auth, isStudent, capturePayment);
-router.post("/verifySignature", verifySignature);
-
-module.exports = router;
