1bdaaad77f14102e3888cad6f05086b694ebc072
Harsh Dugar <dugarharsh2050@gmail.com>
2024-03-22 18:09:49 +0530

    Lect 2-5.5

---
+++ src/pages/CourseDetails.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const CourseDetails = () => {
+
+   const handleBuyCourse = () => {
+      
+   }
+
+  return (
+   <div className=''>
+      
+      <div>
+         <button className='bg-yellow-50 py-3 px-10 mt-10'
+            onClick={() => handleBuyCourse()}
+         >
+            Buy Now
+         </button>
+      </div>
+
+   </div>
+  )
+}
+
+export default CourseDetails
